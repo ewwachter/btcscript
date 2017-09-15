82fcb064d03ff52cdad9becd3f5ca066f1516614
ewwachter <eduardo.wachter@gmail.com>
2017-09-15 20:44:24 +0200

    adding exchange rates for GBP, EUR and USD

--- btcsript.sh
+++ btcsript.sh
@@ -1,5 +1,37 @@
 #!/bin/sh
 
+
+#transferwise exchange rate
+TRWEURBRL=$(curl -s https://transferwise.com/gb/currency-converter/eur-to-brl-rate?amount=1000 | grep " data-rate=\"BRL\">" | grep "?[0-9]*\.[0-9]*" -Eo)
+TRWGBPBRL=$(curl -s https://transferwise.com/gb/currency-converter/gbp-to-brl-rate?amount=1000 | grep " data-rate=\"BRL\">" | grep "?[0-9]*\.[0-9]*" -Eo)
+TRWUSDBRL=$(curl -s https://transferwise.com/gb/currency-converter/usd-to-brl-rate?amount=1000 | grep " data-rate=\"BRL\">" | grep "?[0-9]*\.[0-9]*" -Eo)
+
+GOOGBPBRL=$(curl -s https://finance.google.com/finance?q=GBPBRL | grep  "1 GBP = " | grep "?[0-9]*\.[0-9]*" -Eo)
+GOOEURBRL=$(curl -s https://finance.google.com/finance?q=EURBRL | grep  "1 EUR = " | grep "?[0-9]*\.[0-9]*" -Eo)
+GOOUSDBRL=$(curl -s https://finance.google.com/finance?q=USDBRL | grep  "1 USD = " | grep "?[0-9]*\.[0-9]*" -Eo)
+
+#transferwise fee
+TRWFEE=0.02439
+
+#exchange rate + transferwise fees
+EURBRL=$(echo "scale=3;($GOOEURBRL/(1-$TRWFEE))" | bc)
+GBPBRL=$(echo "scale=3;($GOOGBPBRL/(1-$TRWFEE))" | bc)
+USDBRL=$(echo "scale=3;($GOOUSDBRL/(1-$TRWFEE))" | bc)
+
+
+# echo google USD = $GOOUSDBRL
+# echo google GBP = $GOOGBPBRL
+# echo google EUR = $GOOEURBRL
+
+# echo TW USD = $TRWUSDBRL
+# echo TW GBP = $TRWGBPBRL
+# echo TW EUR = $TRWEURBRL
+
+echo USD = $USDBRL
+echo GBP = $GBPBRL
+echo EUR = $EURBRL
+echo ======================
+
 KRABTCUSD=$(curl -s  https://api.kraken.com/0/public/Ticker?pair=XBTUSD | grep "\"c\":\[\"?[0-9]*\.?[0-9]*" -Eo | grep "?[0-9]*\.?[0-9]*" -Eo)
 KRABTCEUR=$(curl -s  https://api.kraken.com/0/public/Ticker?pair=XBTEUR | grep "\"c\":\[\"?[0-9]*\.?[0-9]*" -Eo | grep "?[0-9]*\.?[0-9]*" -Eo)
 KRABCHUSD=$(curl -s  https://api.kraken.com/0/public/Ticker?pair=BCHUSD | grep "\"c\":\[\"?[0-9]*\.?[0-9]*" -Eo | grep "?[0-9]*\.?[0-9]*" -Eo)
@@ -24,11 +56,21 @@ BTCNEGBRLGBP=$(echo "scale=3;$NEGBTCBRL/$COIBTCGBP" | bc)
 BTCFOXBRLEUR=$(echo "scale=3;$FOXBTCBRL/$KRABTCEUR" | bc)
 BTCNEGBRLEUR=$(echo "scale=3;$NEGBTCBRL/$KRABTCEUR" | bc)
 
-echo BTCFOXBRLUSD = $BTCFOXBRLUSD
-echo BTCNEGBRLUSD = $BTCNEGBRLUSD
+PCTFOXUSD=$(echo "scale=3;($BTCFOXBRLUSD/$USDBRL)-1" | bc)
+PCTNEGUSD=$(echo "scale=3;($BTCNEGBRLUSD/$USDBRL)-1" | bc)
 
-echo BTCFOXBRLGBP = $BTCFOXBRLGBP
-echo BTCNEGBRLGBP = $BTCNEGBRLGBP
+PCTFOXGBP=$(echo "scale=3;($BTCFOXBRLGBP/$GBPBRL)-1" | bc)
+PCTNEGGBP=$(echo "scale=3;($BTCNEGBRLGBP/$GBPBRL)-1" | bc)
 
-echo BTCFOXBRLEUR = $BTCFOXBRLEUR
-echo BTCNEGBRLEUR = $BTCNEGBRLEUR
+PCTFOXEUR=$(echo "scale=3;($BTCFOXBRLEUR/$EURBRL)-1" | bc)
+PCTNEGEUR=$(echo "scale=3;($BTCNEGBRLEUR/$EURBRL)-1" | bc)
+
+
+echo BTCFOXBRLUSD = $BTCFOXBRLUSD \(${PCTFOXGBP}\%\)
+echo BTCNEGBRLUSD = $BTCNEGBRLUSD \(${PCTNEGGBP}\%\)
+
+echo BTCFOXBRLGBP = $BTCFOXBRLGBP \(${PCTFOXEUR}\%\)
+echo BTCNEGBRLGBP = $BTCNEGBRLGBP \(${PCTNEGEUR}\%\)
+
+echo BTCFOXBRLEUR = $BTCFOXBRLEUR \(${PCTFOXEUR}\%\)
+echo BTCNEGBRLEUR = $BTCNEGBRLEUR \(${PCTNEGEUR}\%\)
